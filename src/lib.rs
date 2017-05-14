8c760edf594e038077553ff917be0f2b18fe044e
Christopher Augustus <repos@c4augustus.com>
2017-05-14 15:51:27 +0100

    added rustvr_core dependency and defined PluginHiOvrMobile struct and impls

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+//  Copyright 2017 RustVR
+//
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+extern crate rustvr_core;
+
+pub struct PluginHiOvrMobile {
+}
+
+impl rustvr_core::Plugin for PluginHiOvrMobile {
+}
+
+impl rustvr_core::PluginHi for PluginHiOvrMobile {
+}
+
+pub fn make_plugin() -> PluginHiOvrMobile {
+    PluginHiOvrMobile {}
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
